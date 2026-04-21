0d25ca3f30729a8b14e9b8553cf5317d65f1bfdf
Paul M. Jones <pmjones@pmjones.io>
2026-04-21 16:32:08 -0500

    update README config for new stardoc api

--- config/README.config.php
+++ config/README.config.php
@@ -1,14 +1,16 @@
 <?php return [
+    'namespace' => 'EnvInterop\\Interface\\',
+    'directory' => dirname(__DIR__) . '/src',
     'template' => dirname(__DIR__) . '/resources/README.tpl.md',
-    'files' => [
-        dirname(__DIR__) . "/src/EnvLoaderService.php",
-        dirname(__DIR__) . "/src/EnvParserService.php",
-        dirname(__DIR__) . "/src/EnvSetterService.php",
-        dirname(__DIR__) . "/src/EnvGetter.php",
-        dirname(__DIR__) . "/src/EnvThrowable.php",
-        dirname(__DIR__) . "/src/EnvLoaderThrowable.php",
-        dirname(__DIR__) . "/src/EnvParserThrowable.php",
-        dirname(__DIR__) . "/src/EnvInvalidThrowable.php",
-        dirname(__DIR__) . "/src/EnvTypeAliases.php",
+    'interfaces' => [
+        'EnvLoaderService',
+        'EnvParserService',
+        'EnvSetterService',
+        'EnvGetter',
+        'EnvThrowable',
+        'EnvLoaderThrowable',
+        'EnvParserThrowable',
+        'EnvInvalidThrowable',
+        'EnvTypeAliases',
     ],
 ];
