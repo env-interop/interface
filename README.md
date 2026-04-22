a1ed51e3fcb41ded53d6639688aaf0b0f6f3f045
Paul M. Jones <pmjones@pmjones.io>
2026-04-22 16:40:24 -0500

    remove note about invalidity cross-cutting

--- README.md
+++ README.md
@@ -348,16 +348,6 @@ It adds no class members.
 [_EnvInvalidThrowable_][] interface extends [_EnvThrowable_][] to mark an
 [_Exception_][] as related to environment variable invalidity.
 
-- Notes:
-
-    - **Invalidity is cross-cutting.** Implementations of
-      [_EnvParserService_][] MUST throw this when parsed values fail
-      validation; implementations of [_EnvGetter_][] MAY throw this during
-      constructor validation. Consumers that want to catch all invalidity
-      should catch [_EnvInvalidThrowable_][] (or [_EnvThrowable_][])
-      directly, rather than a narrower sibling such as
-      [_EnvParserThrowable_][].
-
 It adds no class members.
 
 ### _EnvTypeAliases_
--- src/EnvInvalidThrowable.php
+++ src/EnvInvalidThrowable.php
@@ -7,16 +7,6 @@ namespace EnvInterop\Interface;
  * [_EnvInvalidThrowable_][] interface extends [_EnvThrowable_][] to mark an
  * [_Exception_][] as related to environment variable invalidity.
  *
- * - Notes:
- *
- *     - **Invalidity is cross-cutting.** Implementations of
- *       [_EnvParserService_][] MUST throw this when parsed values fail
- *       validation; implementations of [_EnvGetter_][] MAY throw this during
- *       constructor validation. Consumers that want to catch all invalidity
- *       should catch [_EnvInvalidThrowable_][] (or [_EnvThrowable_][])
- *       directly, rather than a narrower sibling such as
- *       [_EnvParserThrowable_][].
- *
  * It adds no class members.
  */
 interface EnvInvalidThrowable extends EnvThrowable
