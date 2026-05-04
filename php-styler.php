4404b4c7717c62e4d8ae793690e46defa96ce8fa
Paul M. Jones <pmjones@pmjones.io>
2026-05-03 22:46:40 -0500

    refresh meta-files

--- php-styler.php
+++ php-styler.php
@@ -5,8 +5,10 @@ use PhpStyler\Format\DeclarationFormat;
 
 return new Config(
     files: new Files(__DIR__ . '/src'),
-    cache: __DIR__ . '/.php-styler.cache',
     format: new DeclarationFormat(
-        lineLen: 80,
+        lineLen: 84,
+        indentLen: 4,
+        indentTab: false,
+        eol: "\n",
     ),
 );
